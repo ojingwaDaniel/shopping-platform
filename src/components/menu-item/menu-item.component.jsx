39d6dcf0be402ec11f136e7011312165dc352c03
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-19 14:09:56 +0100

    added content to my components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import '../menu-item/menu-item.style.css'
-const MenuItem = ({ title, imageUrl,size}) => (
-  <div className={`${size} menu-item`}style={{
-    backgroundImage : `url(${imageUrl})`
-  }}>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="sub-title"> SHOP NOW</span>
