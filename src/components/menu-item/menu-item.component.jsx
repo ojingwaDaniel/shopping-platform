d0c9ba1c0cd0a6f2065fd5ddbef6a2b4f7a0ccfa
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-19 13:10:03 +0100

    added content to my components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-const MenuItem = ({title})=>(
-    <div className="menu-item">
-        <div className="content">
-          <h1 className="title">{title}</h1>
-          <span className="sub-title"> SHOP NOW</span>
-        </div>
+import '../menu-item/menu-item.style.css'
+const MenuItem = ({ title, imageUrl}) => (
+  <div className="menu-item" style={{
+    backgroundImage : `url(${imageUrl})`
+  }}>
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="sub-title"> SHOP NOW</span>
     </div>
-
-)
+  </div>
+);
 export default MenuItem
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,39 +1,9 @@
 import React from "react";
 import "./hompage.style.css";
+import Directory from "../../components/directory/directory.component";
 const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title"> HAT</h1>
-          <span className="sub-title"> SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title"> JACKET</h1>
-          <span className="sub-title"> SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title"> SNEAKERS</h1>
-          <span className="sub-title"> SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title"> WOMEN</h1>
-          <span className="sub-title"> SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title"> MEN</h1>
-          <span className="sub-title"> SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+   <Directory/>
   </div>
 );
 export default HomePage;
