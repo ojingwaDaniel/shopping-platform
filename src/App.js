54c8ceb06e1b8c121762c837f9b5acfc11186ce7
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-20 14:29:11 +0100

    added content to my components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component.jsx'
+import {Route}  from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact  path='\' component={HomePage}/>
+      
     </div>
   );
 }
