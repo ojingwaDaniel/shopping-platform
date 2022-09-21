eaa3a721e29c6c10e0f0369da24c7df791e09609
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-21 15:39:00 +0100

    added content to my components

---
+++ src/components/collection-item/collection-.item.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./collection.item.style.scss"
+const CollectionItem = ({ imageUrl, id, name, price }) => (
+  <div className="collection-item" key={id}>
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    ></div>
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
+  </div>
+);
+export default CollectionItem
--- src/components/collection/collection.preview.jsx
+++ src/components/collection/collection.preview.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import './collection.preview.style.sass'
-const CollectionPreview = ({title,items})=>(
-    <div className="collection-preview">
-        <h1 className="title">{title}</h1>
-        <div className="preview">
-            {
-                items.map(item=>(
-                    <div key={item.id}>{item.name}</div>
-                ))
-            }
-
-
-        </div>
-
+import './collection.preview.style.scss'
+import CollectionItem from "../collection-item/collection-.item.component";
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
-)
+  </div>
+);
 export default CollectionPreview
