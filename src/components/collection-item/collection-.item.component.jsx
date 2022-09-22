dfb8f669a9e6f518d6b989204ed4bda3ae479a8b
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-22 06:45:40 +0100

    added content to my components

--- src/components/collection-item/collection-.item.component.jsx
+++ src/components/collection-item/collection-.item.component.jsx
@@ -10,7 +10,7 @@ const CollectionItem = ({ imageUrl, id, name, price }) => (
     ></div>
     <div className="collection-footer">
       <span className="name">{name}</span>
-      <span className="price">{price}</span>
+      <span className="price">${price}</span>
     </div>
   </div>
 );
