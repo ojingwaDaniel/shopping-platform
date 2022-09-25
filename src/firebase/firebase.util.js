0e861354cf2499d17dcfdd6abc39ca697dda2e13
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-25 13:47:46 +0100

    added content to my components

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -1,16 +1,20 @@
-import firebase from 'firebase/compat/app'
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { initializeApp} from 'firebase/app';
-const Config = {
-  apiKey: "AIzaSyDUJj8W_9YQ90pp_43o2NOPMZQ0mp6Tonw",
-  authDomain: "clothing-shopping-eb2c3.firebaseapp.com",
-  projectId: "clothing-shopping-eb2c3",
-  storageBucket: "clothing-shopping-eb2c3.appspot.com",
-  messagingSenderId: "808256614074",
-  appId: "1:808256614074:web:b0dca7fa944fba315e9c83",
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4t_b_JqbuxZehZHQbb6_wMFN3OVD1nEc",
+  authDomain: "fashion-shopping-97f02.firebaseapp.com",
+  projectId: "fashion-shopping-97f02",
+  storageBucket: "fashion-shopping-97f02.appspot.com",
+  messagingSenderId: "465965338984",
+  appId: "1:465965338984:web:0c3b95e2ac592fe5fa46dd",
+  measurementId: "G-N1Y82KLPTC",
 };
-const app = initializeApp(Config)
- export const auth = getAuth(app)
-const provider = new GoogleAuthProvider()
-provider.setCustomParameters({prompt : 'select_account'})
-export const signInWithGoogle = ()=> firebase.auth.signInWithPopup(provider)
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
