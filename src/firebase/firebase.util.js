a605f14d0c8c30801fc1ea038b6f1bd853ae7185
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-27 19:54:40 +0100

    added content on the firebase utility

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -1,11 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD4t_b_JqbuxZehZHQbb6_wMFN3OVD1nEc",
   authDomain: "fashion-shopping-97f02.firebaseapp.com",
@@ -16,7 +12,6 @@ const firebaseConfig = {
   measurementId: "G-N1Y82KLPTC",
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
