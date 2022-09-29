29623298c7f0556a569e304a67144e25b3b19bb6
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-29 19:16:53 +0100

    added content to my components

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-buttom/custom-buttom.component";
+import './sign-up.style.scss'
