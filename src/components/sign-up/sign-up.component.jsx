e807ef64784771ef5558ea20f73d9cc0472a2113
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-30 03:49:54 +0100

    added content to my components

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,4 +1,23 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-buttom/custom-buttom.component";
-import './sign-up.style.scss'
+import "./sign-up.style.scss";
+import { auth, userProfileDocument } from "../../firebase/firebase.util";
+class SignUp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword : ""
+    };
+  }
+  render(){
+    return(
+        <div className="sign-up">
+            <h1>sig</h1>
+        </div>
+    )
+  }
+}
