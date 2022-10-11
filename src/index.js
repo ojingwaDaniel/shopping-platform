e4d2cd309ca83a4bc58672b5ed3f673e311c1701
ojingwaDaniel <ojingwadanny@gmail.com>
2022-10-11 15:16:02 +0100

    adding redux to my application

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter} from 'react-router-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import store from "./components/redux/store";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider>
+  <Provider store={Store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
 );
-                                               
+
 reportWebVitals();
