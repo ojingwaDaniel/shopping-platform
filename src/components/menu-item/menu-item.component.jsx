fa7da24e76af730cd8bd51e3428382bafbb0d762
ojingwaDaniel <ojingwadanny@gmail.com>
2022-10-28 18:43:59 +0100

    adding redux to my application

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../menu-item/menu-item.style.css'
-import { withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom"
 const MenuItem = ({ title, imageUrl, size,history,match,linkUrl }) => (
   <div className={`${size} menu-item`} onClick = {()=>history.push(`${match.url}${linkUrl}`)}>
     <div className="background-image"
