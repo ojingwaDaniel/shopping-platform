3ce786c91910324c7b2a77f6b47c00b87166ee9b
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-02 14:54:48 +0100

    made changes to the sign in page

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends React.Component{
          <Switch>
            <Route exact path="/" component={HomePage} />
            <Route exact path="/shop" component={ShopPage} />
-           <Route exact path="/signin" component={SignInSignOutPage} />
+           <Route exact path="/signin" render={()=> this.props.currentUser ? } />
          </Switch>
        </div>
      );
