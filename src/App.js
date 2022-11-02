b12b24c6b3727ae94fec299df62f2b6623ef8b45
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-02 14:46:07 +0100

    adding redux to my application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch ,Redirect } from "react-router-dom";
 import Header from './components/header/header.component.jsx'
 import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
