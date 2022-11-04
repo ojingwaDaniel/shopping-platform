5c3f8e9b797c2f0acf765df1bcbc4fbf9178d5da
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-04 01:34:09 +0100

    made changes to the sign in page

--- src/components/cart.icon/cart.component.jsx
+++ src/components/cart.icon/cart.component.jsx
@@ -1,3 +1,12 @@
 import { React } from 'react';
 import './cart.style.scss';
 import {ReactComponent as shoppingIcon} from '../../assets/11.2 shopping-bag.svg.svg'
+ 
+const CartIcon = ()=>(
+    <div className='cart-icon'>
+        <shoppingIcon className = 'shopping-icon'/>
+        <span className='item-count'>0</span>
+
+    </div>
+)
+export default CartIcon
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -26,6 +26,7 @@ const Header = ({ currentUser }) => (
         </Link>
       )}
       
+      
     </div>
   </div>
 );
