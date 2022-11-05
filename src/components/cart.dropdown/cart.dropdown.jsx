78ddbb1870716cde8d2240dada536bbedf712071
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-05 23:17:53 +0100

    added the cart component

--- src/components/cart.dropdown/cart.dropdown.jsx
+++ src/components/cart.dropdown/cart.dropdown.jsx
@@ -3,7 +3,7 @@ import CustomButton from "../custom-buttom/custom-buttom.component";
 import '../cart.dropdown/cart.dropdown.style.scss'
 const CartDropdown = ()=>(
     <div className="cart-dropdown">
-        {/* <div className="cart-items"/> */}
+        <div className="cart-items"/>
         <CustomButton> GO TO CHECKOUT</CustomButton>
 
 
