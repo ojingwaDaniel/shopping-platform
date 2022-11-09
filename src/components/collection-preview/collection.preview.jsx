f7853c119bb2c52e3a3b1bdeb7677a54fde3085b
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-09 19:48:50 +0100

    added the cart component

--- src/components/collection-preview/collection.preview.jsx
+++ src/components/collection-preview/collection.preview.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import './collection.preview.style.scss'
 import CollectionItem from "../collection-item/collection-.item.component"
 const CollectionPreview = ({ title, items }) => (
