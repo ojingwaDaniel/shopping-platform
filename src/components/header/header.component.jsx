2488c6b65a1388a4f52df35df4b2294be43364e5
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-11 12:20:41 +0100

    added the cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 import { ReactComponent as Logo} from '../../assets/4.4 crown.svg.svg'
 import './header.style.scss'
 import { auth } from "../../firebase/firebase.util"
