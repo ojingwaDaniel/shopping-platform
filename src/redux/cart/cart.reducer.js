06106e18d6ae445248711792e46c8104742f0e7e
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-23 11:28:51 +0100

    added a cart toggle reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,10 +1,10 @@
-
+import cartActionType from "./cart.action"
 const INITIAL_STATE = {
     hidden : true
 }
 const CartReducer = (state = INITIAL_STATE, action)=>{
     switch(action.type){
-        case 'TOGGLE_CART_HIDDEN':
+        case cartActionType.TOGGLE_CART_HIDDEN:
         return{
             ...state,
             hidden : !state.hidden
