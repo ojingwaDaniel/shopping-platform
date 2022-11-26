505a8490a33380db551cd8e915ab2a3dfd648946
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-26 21:00:18 +0100

    added the cart component

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,4 +1,4 @@
-const CartActionType = {
-  TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
-};
-export default CartActionType
+// const CartActionType = {
+//   TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+// };
+// export default CaartActionType
