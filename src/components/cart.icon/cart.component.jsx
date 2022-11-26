18974fbbd5a94777cde9db6b367b0edb3c816cf8
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-26 21:07:14 +0100

    added the cart component

--- src/components/cart.icon/cart.component.jsx
+++ src/components/cart.icon/cart.component.jsx
@@ -1,6 +1,6 @@
 import { React } from 'react';
 import { connect } from 'react-redux';
-// import { ToggleCartHidden } from '../../redux/cart/cart.action';
+import { ToggleCartHidden } from '../../redux/cart/cart.action';
 
 import './cart.style.scss';
 import {ReactComponent as ShoppingIcon} from '../../assets/11.2 shopping-bag.svg.svg'
