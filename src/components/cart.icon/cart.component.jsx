6585b9d42303e24902e48a2790fc6ce1910775d5
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-27 17:58:48 +0100

    added the cart component

--- src/components/cart.icon/cart.component.jsx
+++ src/components/cart.icon/cart.component.jsx
@@ -13,7 +13,7 @@ const CartIcon = ({ToggleCartHidden})=>(
     </div>
 )
 const mapDispatchToProps = dispatch =>({
-    toggleCartHidden : dispatch(ToggleCartHidden())
+    ToggleCartHidden : ()=> dispatch(ToggleCartHidden())
     
 
 })
