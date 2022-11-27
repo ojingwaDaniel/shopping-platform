741726f158878374c06af119dd5b782fd33063f5
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-27 19:08:51 +0100

    added the cart component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,14 +1,14 @@
-import cartActionType from "./cart.action"
+import CartActionType from "./cart.types";
 const INITIAL_STATE = {
     hidden : true
 }
 const CartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case cartActionType.TOGGLE_CART_HIDDEN:
+    case CartActionType.TOGGLE_CART_HIDDEN:
       return {
         ...state,
         hidden: !state.hidden
-      };
+      }
     default:
       return {
         state
