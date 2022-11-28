a0e88c46fe4aaf21320d90364ae2be56120decab
ojingwaDaniel <ojingwadanny@gmail.com>
2022-11-28 20:15:09 +0100

    added the cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo} from '../../assets/4.4 crown.svg.svg';
 import './header.style.scss';
