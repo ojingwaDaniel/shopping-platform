1217dd6202574348211346919fc6a3405f6e350c
ojingwaDaniel <ojingwadanny@gmail.com>
2022-12-27 07:15:59 +0100

    added the cart component

--- src/components/collection-item/collection-.item.component.jsx
+++ src/components/collection-item/collection-.item.component.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import "./collection.item.style.scss"
-import CustomButton from "../custom-buttom/custom-buttom.component"
+import CustomButton from "../custom-buttom/custom-buttom.component";
 const CollectionItem = ({ imageUrl, id, name, price }) => (
   <div className="collection-item" key={id}>
     <div
