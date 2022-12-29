4f07b7252aba6ca4ab9a4fe062334ca642d1aa96
ojingwaDaniel <ojingwadanny@gmail.com>
2022-12-29 13:17:46 +0100

    added the cart component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch ,Redirect } from "react-router-dom";
 import Header from './components/header/header.component.jsx';
-import HomePage from './pages/homepage/homepage.component.jsx'
+import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx'
 import SignInSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import './App.css'
