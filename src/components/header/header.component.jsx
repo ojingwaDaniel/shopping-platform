9b8dd4483b523f3354a3c6080de918fbc4b6c382
ojingwaDaniel <ojingwadanny@gmail.com>
2023-01-09 23:34:29 +0100

    added the cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { ReactComponent as Logo} from '../../assets/4.4 crown.svg.svg';
+import { ReactComponent as Logo} from '../../assets/4.4 crown.svg.svg'
 import './header.style.scss';
 import { auth } from "../../firebase/firebase.util";
 import { connect } from "react-redux"
