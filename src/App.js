988d686c4b7e724fee3748eb0d32233fc3665652
ojingwaDaniel <ojingwadanny@gmail.com>
2023-01-15 08:27:40 +0100

    added the cart component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/header.component.jsx';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
-import './App.css'
+import './App.css';
 import { auth ,userProfileDocument} from './firebase/firebase.util'
 import { connect } from 'react-redux'
 import { setCurrentUser } from './redux/user/users.actions.js'
