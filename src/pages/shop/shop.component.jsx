e679b9e8b52162ea188e9c825fe7345e5d7f195e
ojingwaDaniel <ojingwadanny@gmail.com>
2023-02-04 23:45:35 +0100

    added the cart component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ShopData from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection.preview";
+import CollectionPreview from "../../components/collection-preview/collection.preview"
 class ShopPage extends React.Component{
     constructor(props){
         super(props)
