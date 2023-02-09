b6be781e7cd2d977ec4be911e3f9961259f00e64
ojingwaDaniel <ojingwadanny@gmail.com>
2023-02-09 19:18:17 +0100

    added the cart component

--- src/components/cart-item/cart.item.component.jsx
+++ src/components/cart-item/cart.item.component.jsx
@@ -1,3 +1,13 @@
 import React from 'react'
 import './cart.item.style.scss'
-const cartItem = ({item : {imageUrl,name,price,quantity}})=>()
+const cartItem = ({item : {imageUrl,name,price,quantity}})=>(
+       <div className='cart-item'>
+         <img src = {imageUrl} alt = 'item'/> 
+         <div className = 'item-details'>
+            <spam className = 'name'> {name}<spam/>
+            <spam className = 'price'>{quantity} x ${price}<spam/>
+            <div/>
+       </div>
+)
+   
+export default cartItem
